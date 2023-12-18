aa0a3a810e997929cd89c230e831846afc2278f4
MonkeyBuisness <85140465+Siddynexp@users.noreply.github.com>
2023-12-17 21:05:04 -0600

    Add files via upload

---
+++ dice.py
@@ -0,0 +1,7 @@
+from microbit import *
+import random
+
+
+while True:
+    if accelerometer.was_gesture('shake'):
+        display.show(random.randint(1, 6))
---
+++ stepsCounter.py
@@ -0,0 +1,9 @@
+from microbit import *
+
+
+steps=0
+
+while True:
+    if accelerometer.was_gesture('shake'):
+        steps += 1
+        display.show(steps)
